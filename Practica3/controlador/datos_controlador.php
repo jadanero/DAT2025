2b6cb5c6e4a3b4eb8d63fcc872abadcdd05b9f14
jadanero <jadaneroc@gmail.com>
2025-11-11 19:29:36 +0100

    Changes

--- Practica3/controlador/datos_controlador.php
+++ Practica3/controlador/datos_controlador.php
@@ -1,6 +1,14 @@
 <?php
+$input = file_get_contents("php://input");
+$data = json_decode($input, true);
 
+if ($data === null) {
+    http_response_code(400);
+    echo "JSON inválido";
+    exit();
+}
 
-
-
+// Añadir marca de tiempo
+$data["recibido_en"] = date("Y-m-d H:i:s");
+echo "Controlador ha recibido: " . json_encode($data) . "\n";
 ?>
